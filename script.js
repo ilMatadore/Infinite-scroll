fc43b03348cfed8855c47f12c5c892b42735bbac
ilMatadore <restarter.dev@gmail.com>
2020-06-30 09:21:03 -0500

    uploading project

---
+++ script.js
@@ -0,0 +1,89 @@
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+
+// Unsplash API
+
+let count = 5;
+const proxyUrl = "https://radiant-castle-44157.herokuapp.com/";
+const apiKey = "60T9-RSltDcTQra0zVTEvVumNJg7WhyrIvaZxgI6Lac";
+let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}&query=travel`;
+
+// Check if all images were loaded
+imageLoaded = () => {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+    count = 30;
+  }
+};
+
+// Helper function to set attributes on DOM elements
+
+setAttributes = (element, attributes) => {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+};
+
+// Create Elements for Links & photeos, ADD to DOM
+
+displayPhotos = () => {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+  // Run function for each object in photosArray
+  photosArray.forEach((photo) => {
+    //Create <a> to link to Unsplash
+    const item = document.createElement("a");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+
+    // Create <img> for photo
+    const img = document.createElement("img");
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+
+    // Event listener, check when each is finished loading
+    img.addEventListener("load", imageLoaded);
+
+    // Put <img> inside <a>, then both inside image container element
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+};
+
+// Get photos from Unsplash API
+
+async function getPhotos() {
+  try {
+    const response = await fetch(proxyUrl + apiUrl);
+    photosArray = await response.json();
+    displayPhotos();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Check to see if scrolling near bottom of page, Load More Photos
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
+});
+
+// On load
+getPhotos();
